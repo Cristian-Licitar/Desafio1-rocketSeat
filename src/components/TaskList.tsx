e205a2a249eecba61af44d28e0a83c154839a115
cristain-GeniusGroup <allancristian787@gmail.com>
2023-10-01 22:09:08 -0300

    Commit com o ID sendo gerado pela UUID

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
 interface Task {
-  id: number;
+  id: string;
   title: string;
   isComplete: boolean;
 }
@@ -16,14 +16,44 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+   
+      if(newTaskTitle.trim() !== ''){
+        const newTask: Task = {
+          id: uuidv4(),
+          title: newTaskTitle,
+          isComplete: false
+        }
+        //É preciso usar o operador Spread que são os 3 pontos(...tasks) para passar o array já existente no primeiro parâmetro
+        setTasks([...tasks, newTask]);
+        setNewTaskTitle('');
+
+      } else {
+        return console.error('A task não pode ser criada sem título');
+      }
+      console.log(tasks);
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  function handleToggleTaskCompletion(id: string) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    //primeiro eu percorro o arrray de tasks para achar o objeto que desejo atualizar
+    const updateTasksStatus = tasks.map(task => {
+      if(task.id === id){//verifico se alguns dos objetos da lista tem o ID passado
+        //retorno para a variável criada o array atualizado
+        return { ...task, isComplete : !task.isComplete };//E o objeto em questão terá a variável isComplete com o seu valor alterado
+      } else {
+        return task//Em caso de não encontrar um objeto com ID, só devolvo a lista de tasks
+      }
+    });
+
+    setTasks(updateTasksStatus);//Passo para o useState a lista atualizada
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: string) {
     // Remova uma task da listagem pelo ID
+    // Primeiro eu crio um objeto qualquer (deleteTask) que irá receber o valor retornado do metódo filter()
+    setTasks((deleteTask) => deleteTask.filter(task => task.id !== id));
+    // O filter() só ira devolver os objetos com o ID diferente do que eu quero excluir
   }
 
   return (
